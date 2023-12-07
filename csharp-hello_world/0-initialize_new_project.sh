6e8034e1e8765cf980933ee197dfd5abe5c50ece
Jonathan <jonathanperez604@gmail.com>
2023-12-06 22:42:53 -0400

    Initialize new project

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# Create a new folder titled 0-new_project
-mkdir 0-new_project
-
-# Change to the newly created folder
-cd 0-new_project
-
-# Initialize a new C# console application using dotnet
-dotnet new console -n 0-new_project
-
-# Perform post-creation actions: dotnet restore
-dotnet restore 0-new_project.csproj
+mkdir ./0-new_project
+cd ./0-new_project
+dotnet new console
