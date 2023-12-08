1c83f7488b11fbef6c857f73fb7432f8b1f09dd1
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-08 16:53:24 -0400

    Play with strings

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string str1 = "Welcome to";
-        string str2 = "Holberton School!";
-        Console.WriteLine($"{str1} {str2}");
+        string str2 = " Holberton School!";
+        Console.WriteLine($"{str1}{str2}");
     }
 }
