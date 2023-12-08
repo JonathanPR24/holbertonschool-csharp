38945b1d57a2ffee7795c52c49fec1f52f9e2431
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-08 17:14:38 -0400

    Print variables

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int streetNumber = 972;
+        string streetName = "Mission Street";
+
+        Console.WriteLine("{0} {1}", streetNumber, streetName);
+    }
+}
