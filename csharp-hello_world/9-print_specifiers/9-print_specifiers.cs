6e24d1c1c63b6ca0746304c5f246ee818fc4662d
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-08 23:09:09 -0400

    Print specifiers

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = 75.53;
+        int currency = 9876543;
+
+        Console.WriteLine("Percent: {0:F2}% tip", percent);
+        Console.WriteLine("Currency: ${0:N2}", currency / 100.0);
+    }
+}
