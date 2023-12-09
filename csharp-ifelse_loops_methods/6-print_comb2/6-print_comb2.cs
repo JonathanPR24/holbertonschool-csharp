af604a5a9bf54aabad785a3f7715b7fc8d7445e5
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-09 19:56:57 -0400

    Prints all possible different combinations of two digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                Console.Write($"{i}{j}{(i == 8 && j == 9 ? "\n" : ", ")}");
+            }
+        }
+    }
+}
