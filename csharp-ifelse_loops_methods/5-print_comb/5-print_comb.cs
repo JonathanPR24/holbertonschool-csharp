30b4dcf177172ce1d8406d55202f5c3d7dc8b48a
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-09 19:51:14 -0400

    Prints numbers from 0 to 99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number < 100; number++)
+        {
+            Console.Write($"{number:D2}");
+
+            if (number < 99)
+            {
+                Console.Write(", ");
+            }
+            else
+            {
+                Console.Write("\n");
+            }
+        }
+    }
+}
