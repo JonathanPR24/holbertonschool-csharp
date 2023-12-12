9a9704ad7678c5ccdfd6c5de883f0fcdceb96b63
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-12 18:26:50 -0400

    Print an array of integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+        }
+
+        Console.WriteLine(size == 0 ? "" : string.Join(" ", newArray));
+
+        return newArray;
+    }
+}
