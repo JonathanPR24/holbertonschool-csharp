219743dae531a3bd7411ba054fd8c1ebd5a7c3d3
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-13 01:18:12 -0400

    Replace element

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,37 +4,12 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index < 0 || index >= array.Length)
+        if (index >= array.Length || index < 0)
         {
             Console.WriteLine("Index out of range");
             return array;
         }
-
         array[index] = n;
         return array;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] array = { 0, 1, 2, 3, 4, 5, 6 };
-
-        PrintArray(array);
-        Array.ReplaceElement(array, 1, 98);
-        PrintArray(array);
-    }
-
-    static void PrintArray(int[] array)
-    {
-        for (int i = 0; i < array.Length; i++)
-        {
-            Console.Write(array[i]);
-            if (i < array.Length - 1)
-                Console.Write(" ");
-        }
-
-        Console.WriteLine();
     }
 }
