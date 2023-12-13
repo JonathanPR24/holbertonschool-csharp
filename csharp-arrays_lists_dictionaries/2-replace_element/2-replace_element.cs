1159814a67983a6b261f96af185a634359b259ba
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-13 00:59:21 -0400

    Replace element

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write($"{array[i]}{(i == 0 ? "" : " ")}");
+        }
+        Console.WriteLine();
+    }
+}
