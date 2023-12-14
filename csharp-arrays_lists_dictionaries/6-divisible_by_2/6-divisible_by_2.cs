756347a925e6f0e312dc60eb07ef78bc686886e3
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-14 16:20:19 -0400

    Only by 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("List is null");
+            return null;
+        }
+
+        List<bool> result = new List<bool>();
+
+        foreach (int num in myList)
+        {
+            result.Add(num % 2 == 0);
+        }
+
+        return result;
+    }
+}
