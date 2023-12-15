f0bd24389972774658665f1ba1daab7bc3cac481
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-14 22:29:54 -0400

    Best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        string bestStudent = "";
+        int maxScore = int.MinValue;
+
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > maxScore)
+            {
+                maxScore = entry.Value;
+                bestStudent = entry.Key;
+            }
+        }
+
+        return bestStudent;
+    }
+}
