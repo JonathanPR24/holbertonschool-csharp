c12383111093fcf5972990f0ddff5608ff15cb27
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-14 22:53:57 -0400

    Rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+
+        array[2, 2] = 1;
+
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j]);
+
+                if (j < array.GetLength(1) - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
