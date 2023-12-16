1dc8e0908922d5eb4398e108c8fa0b25f91884fc
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-16 19:00:05 -0400

    Queue it up

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count > 0)
+        {
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("Queue is empty");
+        }
+
+        aQueue.Enqueue(newItem);
+
+        bool containsSearch = aQueue.Contains(search);
+        Console.WriteLine($"Queue contains \"{search}\": {containsSearch}");
+
+        if (containsSearch)
+        {
+            while (aQueue.Count > 0 && aQueue.Dequeue() != search) { }
+        }
+
+        return aQueue;
+    }
+}
