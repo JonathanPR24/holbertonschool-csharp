4777007acfb04718b217f192b66add407ca495f5
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-16 01:18:51 -0400

    Unique addition

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        // Use LINQ to filter out duplicate elements and then calculate the sum
+        return myList.Distinct().Sum();
+    }
+}
