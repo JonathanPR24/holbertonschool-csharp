966a391868877db5e1173c8b2dd069c9672e1c9f
Jonathan Perez <jonathanperez604@gmail.com>
2023-12-16 18:55:18 -0400

    Full stack

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,25 +6,12 @@ class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine($"Number of items: {aStack.Count}");
-        Console.WriteLine($"Top item: {aStack.Count > 0 ? aStack.Peek() : "Stack is empty"}");
+        Console.WriteLine($"Top item: {(aStack.Count > 0 ? aStack.Peek() : "Stack is empty")}");
         Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
 
-        if (aStack.Contains(search))
-        {
-            Stack<string> tempStack = new Stack<string>();
-
-            while (aStack.Count > 0)
-            {
-                string item = aStack.Pop();
-                if (item == search) break;
-                tempStack.Push(item);
-            }
-
-            while (tempStack.Count > 0) aStack.Push(tempStack.Pop());
-        }
+        while (aStack.Count > 0 && aStack.Pop() != search) { }
 
         aStack.Push(newItem);
-
         return aStack;
     }
 }
