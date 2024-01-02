bf9fc0eb92f905790e3f94bb137557ba7233a340
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-02 15:05:46 -0400

    Integer division

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {(b == 0 ? 0 : a / b)}");
+        }
+    }
+}
