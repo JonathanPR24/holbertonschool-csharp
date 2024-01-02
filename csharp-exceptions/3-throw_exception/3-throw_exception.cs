0e44717b723f0c261dccd60ce5cd19d23a6ae5f9
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-02 18:12:56 -0400

    Throw exception

---
+++ csharp-exceptions/3-throw_exception/3-throw_exception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Except
+{
+    public static void Throw()
+    {
+        throw new Exception("Custom Exception");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Except.Throw();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Exception raised");
+        }
+    }
+}
