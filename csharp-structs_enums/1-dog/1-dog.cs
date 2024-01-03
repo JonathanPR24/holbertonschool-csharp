842e5228fa79fc92ac9904ef8925a3b08cec5e1c
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-03 15:51:27 -0400

    Define a new struct Dog

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Define the Rating enum
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+// Define the Dog struct with public members
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
