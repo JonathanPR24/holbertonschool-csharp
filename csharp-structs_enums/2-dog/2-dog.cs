21f83b729061c400675c83f2517855e49730dafd
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-03 15:54:46 -0400

    Add a constructor to struct Dog

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Define the Rating enum
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+// Define the Dog struct with a constructor
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    // Constructor with parameters
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+}
