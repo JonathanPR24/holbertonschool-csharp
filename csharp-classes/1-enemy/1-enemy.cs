b7820ee32f0b1f10c6f798695964d873ae5b830b
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-10 14:17:51 -0400

    Healthy competition

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// 1-enemy.cs
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// Sets the value of health to 0.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
