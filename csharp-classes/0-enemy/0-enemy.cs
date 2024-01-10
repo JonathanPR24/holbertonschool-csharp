c42165d1ba4b5ed99504006228224c0f8447366a
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-10 13:57:23 -0400

    Namespace Enemies

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+// 0-enemy.cs
+namespace Enemies
+{
+    public class Zombie
+    {
+        // You can add properties, methods, or other members to the Zombie class if needed.
+    }
+}
