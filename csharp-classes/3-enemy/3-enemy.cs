bbc80f317361156dc2c7f1a486316b2a100969cd
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-10 14:28:50 -0400

    Zombie health

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,48 @@
+﻿// 3-enemy.cs
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie.
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// Sets the value of health to 0.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with a specified health value.
+        /// </summary>
+        /// <param name="value">The health value for the zombie.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+
+            this.health = value;
+        }
+
+        /// <summary>
+        /// Gets the health value of the zombie.
+        /// </summary>
+        /// <returns>The health value of the zombie.</returns>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
