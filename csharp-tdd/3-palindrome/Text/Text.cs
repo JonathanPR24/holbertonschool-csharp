8418b79acdb08ea7cbc332655c3729ebf30ac07d
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-11 16:27:08 -0400

    Desserts, I stressed!

---
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Text
+{
+    public static class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            // Remove spaces and punctuation, and convert to lowercase
+            string cleanedString = Regex.Replace(s, @"[^a-zA-Z]", "").ToLower();
+
+            // Compare the cleaned string with its reverse
+            return cleanedString == ReverseString(cleanedString);
+        }
+
+        private static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
