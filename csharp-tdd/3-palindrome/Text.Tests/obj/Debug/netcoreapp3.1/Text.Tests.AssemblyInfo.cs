12b61c0ed236eb469531105bdaa68605533529c5
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-11 19:56:05 -0400

    Desserts, I stressed!

--- csharp-tdd/3-palindrome/Text.Tests/obj/Debug/netcoreapp3.1/Text.Tests.AssemblyInfo.cs
+++ csharp-tdd/3-palindrome/Text.Tests/obj/Debug/netcoreapp3.1/Text.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3613823dc4c9a82fbecc10a7bed558d8b5cb7e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4b79658f2ad2831858851534826571ff7e4f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
