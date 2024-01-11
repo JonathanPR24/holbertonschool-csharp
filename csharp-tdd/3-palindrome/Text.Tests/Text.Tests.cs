2c4b79658f2ad2831858851534826571ff7e4f08
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-11 19:53:12 -0400

    Desserts, I stressed!

--- csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,36 +1,24 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace Text.Tests
 {
     public class StrTests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase("Racecar", true)] // Regular palindrome
+        [TestCase("level", true)] // Uppercase + lowercase letters
+        [TestCase("A man, a plan, a canal: Panama.", true)] // Punctuation
+        [TestCase("Never odd or even", true)] // Spaces
+        [TestCase("", true)] // Empty string
+        [TestCase("hello", false)] // Non-palindrome
+        [TestCase("Palindrome", false)] // Non-palindrome
+        [TestCase("Not a palindrome", false)] // Non-palindrome
+        public void IsPalindrome_ValidInput_ReturnsExpected(string input, bool expected)
         {
-        }
+            // Act
+            bool result = Str.IsPalindrome(input);
 
-        [TestCase("abcba", TestName = "IsPalindrome_SingleWordPalindrome_ReturnsTrue")]
-        [TestCase("BananasananaB", TestName = "IsPalindrome_MixedCasePalindrome_ReturnsTrue")]
-        [TestCase("fubaRabuf", TestName = "IsPalindrome_MixedCaseNonPalindrome_ReturnsFalse")]
-        [TestCase("", TestName = "IsPalindrome_EmptyString_ReturnsTrue")]
-        [TestCase("Beneb", TestName = "IsPalindrome_SingleWordNonPalindrome_ReturnsFalse")]
-        [TestCase("Ben!neB", TestName = "IsPalindrome_NonAlphanumericCharacters_ReturnsTrue")]
-        [TestCase("f o o oof", TestName = "IsPalindrome_WhitespacePalindrome_ReturnsTrue")]
-        [TestCase("A man, a plan, a canal: Panama.", TestName = "IsPalindrome_ComplexPalindrome_ReturnsTrue")]
-        public void IsPalindrome_ValidInput_ReturnsTrue(string input)
-        {
-            bool result = Text.Str.IsPalindrome(input);
-
-            Assert.IsTrue(result, $"Expected true for input: '{input}'");
-        }
-
-        [TestCase("Juan David", TestName = "IsPalindrome_NonPalindromeWithSpace_ReturnsFalse")]
-        [TestCase("AbCd", TestName = "IsPalindrome_MixedCaseNonPalindrome_ReturnsFalse")]
-        public void IsPalindrome_NonPalindrome_ReturnsFalse(string input)
-        {
-            bool result = Text.Str.IsPalindrome(input);
-
-            Assert.IsFalse(result, $"Expected false for input: '{input}'");
+            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
--- csharp-tdd/3-palindrome/Text/obj/Debug/netstandard2.0/Text.AssemblyInfo.cs
+++ csharp-tdd/3-palindrome/Text/obj/Debug/netstandard2.0/Text.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d62a6ecbfc8259c12887890076197ee05037c20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3613823dc4c9a82fbecc10a7bed558d8b5cb7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
