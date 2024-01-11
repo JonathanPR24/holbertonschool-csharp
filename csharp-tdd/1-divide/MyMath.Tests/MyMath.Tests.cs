3bb40189f43d6b6dd9ad594f2314e11f9a277338
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-11 15:07:32 -0400

    Divide a matrix

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,47 @@
 using NUnit.Framework;
+using MyMath;
 
-namespace Tests
+namespace MyMath.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class MyMathTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void RegularDivision()
         {
+            int[,] matrix = { { 4, 8 }, { 12, 16 } };
+            int num = 2;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.AreEqual(2, result[0, 0]);
+            Assert.AreEqual(4, result[0, 1]);
+            Assert.AreEqual(6, result[1, 0]);
+            Assert.AreEqual(8, result[1, 1]);
         }
 
         [Test]
-        public void Test1()
+        public void DivideByZero()
         {
-            Assert.Pass();
+            int[,] matrix = { { 4, 8 }, { 12, 16 } };
+            int num = 0;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.IsNull(result);
+            // You may want to add additional assertions based on the expected behavior.
+        }
+
+        [Test]
+        public void MatrixIsNull()
+        {
+            int[,] matrix = null;
+            int num = 2;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.IsNull(result);
+            // You may want to add additional assertions based on the expected behavior.
         }
     }
 }
