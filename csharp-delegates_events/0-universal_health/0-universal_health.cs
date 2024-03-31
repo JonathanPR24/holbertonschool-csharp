7bebb69bd59ae58470e09a3a74f9134e3cea2fa6
Jonathan <jonathanperez604@gmail.com>
2024-03-31 19:08:21 -0400

    Universal health

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player1 = new Player("Electric Mouse", 500f);
+        Player player2 = new Player("Water Turtle");
+
+        player1.PrintHealth();
+        player2.PrintHealth();
+    }
+}
