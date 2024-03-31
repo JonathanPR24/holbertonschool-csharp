2523c489eb7e8b606c86eedc83470202d54a1061
Jonathan <jonathanperez604@gmail.com>
2024-03-31 12:35:36 -0400

    Queue

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Represents a generic queue.
+/// </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Checks the type of the queue.
+    /// </summary>
+    /// <returns>The type of the queue.</returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+}
