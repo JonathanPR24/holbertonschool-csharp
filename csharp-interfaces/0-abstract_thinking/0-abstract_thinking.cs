c896a8933cae9b2b87da291a7fc32ad7852ee6ad
Jonathan <jonathanperez604@gmail.com>
2024-03-31 13:51:40 -0400

    Abstract thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Abstract class representing a base entity.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name of the entity.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Overrides the ToString method to provide custom string representation.
+    /// </summary>
+    /// <returns>A string representing the name and type of the entity.</returns>
+    public override string ToString()
+    {
+        return $"{Name} is a {GetType().Name}";
+    }
+}
