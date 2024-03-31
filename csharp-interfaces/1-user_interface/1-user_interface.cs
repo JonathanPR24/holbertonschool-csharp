dc586dc4a9d9ee198541691f0a0f2047fe141268
Jonathan <jonathanperez604@gmail.com>
2024-03-31 16:40:44 -0400

    User interface

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,57 +1,49 @@
 ﻿using System;
 
-///<summary>Abstract base class representing an entity.</summary>
-abstract class Base
+public abstract class Base
 {
-    public string name = "";
+    public string name { get; set; } = "";
 
     public override string ToString()
     {
-        return (this.name + " is a " + this.GetType().ToString());
+        return $"{name} is a {this.GetType()}";
     }
 }
 
-///<summary>Interface for interactive objects.</summary>
-interface IInteractive
+public interface IInteractive
 {
     void Interact();
 }
 
-///<summary>Interface for breakable objects.</summary>
-interface IBreakable
+public interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
 
-///<summary>Interface for collectable objects.</summary>
-interface ICollectable
+public interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
 
-///<summary>Class representing a test object that inherits from Base class and all interfaces.</summary>
-class TestObject : Base, IInteractive, IBreakable, ICollectable
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
     public bool isCollected { get; set; }
 
-    ///<summary>Method invoked when the object interacts with something.</summary>
     public void Interact()
     {
-
+        // Provide implementation for interaction
     }
 
-    ///<summary>Method invoked when the object breaks.</summary>
     public void Break()
     {
-
+        // Provide implementation for breaking
     }
 
-    ///<summary>Method invoked when the object is collected.</summary>
     public void Collect()
     {
-
+        // Provide implementation for collecting
     }
 }
