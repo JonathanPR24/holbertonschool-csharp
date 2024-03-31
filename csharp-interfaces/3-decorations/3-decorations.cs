38c6de0194df2510adff3d1e74f6496872e5c114
Jonathan <jonathanperez604@gmail.com>
2024-03-31 18:30:42 -0400

    Interior decorating

--- csharp-interfaces/3-decorations/3-decorations.cs
+++ csharp-interfaces/3-decorations/3-decorations.cs
@@ -1,88 +1,126 @@
 ﻿using System;
 
 /// <summary>
-/// Represents a decoration object that can be interacted with and broken.
+/// Represents a decorative object in the game environment, which can be interacted with and may have durability and quest item properties.
 /// </summary>
-class Decoration : Base, IInteractive, IBreakable
-{
-    public int durability { get; set; }
+class Decoration : Base, IInteractive, IBreakable {
     public bool isQuestItem { get; set; }
+    public int durability { get; set; }  
 
-    /// <summary>
-    /// Constructor for the Decoration class.
-    /// </summary>
-    /// <param name="name">The name of the decoration.</param>
-    /// <param name="durability">The durability of the decoration.</param>
-    /// <param name="isQuestItem">Indicates if the decoration is a quest item.</param>
-    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
-    {
-        if (durability <= 0)
-        {
-            throw new ArgumentException("Durability must be greater than 0");
+    public Decoration(string name = "Decoration", int durability = 1,
+                       bool isQuestItem = false) {
+
+        if (durability <= 0) {
+            throw new Exception("Durability must be greater than 0");
+        } else {
+            this.name = name;
+            this.durability = durability;
+            this.isQuestItem = isQuestItem;
         }
 
-        this.name = name;
-        this.durability = durability;
-        this.isQuestItem = isQuestItem;
     }
 
-    /// <summary>
-    /// Interact with the decoration.
-    /// </summary>
-    public void Interact()
-    {
-        if (durability <= 0)
-        {
-            Console.WriteLine($"The {name} has been broken.");
-            return;
-        }
+    public void Interact() {
 
-        if (isQuestItem)
-        {
+        if (durability <= 0) {
+            Console.WriteLine($"The {name} has been broken.");
+        } else if (isQuestItem == true) {
             Console.WriteLine($"You look at the {name}. There's a key inside.");
-        }
-        else
-        {
+        } else {
             Console.WriteLine($"You look at the {name}. Not much to see here.");
         }
+
     }
 
-    /// <summary>
-    /// Break the decoration.
-    /// </summary>
-    public void Break()
-    {
-        if (durability > 0)
-        {
-            durability--;
+    public void Break() {
 
-            if (durability > 0)
-            {
-                Console.WriteLine($"You hit the {name}. It cracks.");
-            }
-            else
-            {
-                Console.WriteLine($"You smash the {name}. What a mess.");
-            }
-        }
-        else
-        {
+        durability--;
+
+        if (durability > 0) {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        } else if (durability == 0) {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        } else {
             Console.WriteLine($"The {name} is already broken.");
         }
+
     }
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Decoration coffeeMug = new Decoration("Coffee Mug", 2);
 
-        Console.WriteLine(coffeeMug.ToString());
+/// <summary>
+/// Defines an interactable door.
+/// </summary>
+class Door : Base, IInteractive {
 
-        coffeeMug.Interact();
-        coffeeMug.Break();
-        coffeeMug.Break();
-        coffeeMug.Interact();
+    public Door(string _name = "Door") {
+        name = _name;
     }
+
+    public void Interact() {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+
+}
+
+/// <summary>
+/// A base class.
+/// </summary>
+abstract class Base {
+
+    public string name { get; set; }
+
+    /// <summary>
+    /// Convert the object to a string.
+    /// </summary>
+    public override string ToString() {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
+
+/// <summary>
+/// Interface defining an interactable object.
+/// </summary>
+public interface IInteractive {
+
+    /// <summary>
+    /// Interact with this object.
+    /// </summary>
+    void Interact();
+
+}
+
+/// <summary>
+/// Interface defining a breakable object.
+/// </summary>
+public interface IBreakable {
+
+    /// <summary>
+    /// The durability of the breakable object.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Break this object.
+    /// </summary>
+    void Break();
+
+}
+
+/// <summary>
+/// Interface defining a collectable object.
+/// </summary>
+public interface ICollectable {
+
+    /// <summary>
+    /// Denotes whether the object has been collected.
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Collect this object.
+    /// </summary>
+    void Collect();
+
 }
