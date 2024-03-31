cf18c5071d63e3a77c85ee7417b7ae6ca0cb2b30
Jonathan <jonathanperez604@gmail.com>
2024-03-31 16:18:02 -0400

    User interface

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,73 +1,41 @@
 ﻿using System;
 
-///<summary>Base class</summary>
-abstract class Base
-{
-    public string name = "";
-
-    public override string ToString()
-    {
-        return $"{name} is a {GetType()}";
-    }
-}
-
-///<summary>Interactive interface</summary>
 interface IInteractive
 {
     void Interact();
 }
 
-///<summary>Breakable interface</summary>
 interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
 
-///<summary>Collectable interface</summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
 
-///<summary>Class that inherits from Base class and all interfaces</summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
+    // Properties from IBreakable interface
     public int durability { get; set; }
-    public bool isCollected { get; set; }
-
-    public void Interact()
-    {
-        // Implementation for Interact method
-    }
-
     public void Break()
     {
-        // Implementation for Break method
+        // Implementation not provided for this task
     }
 
+    // Properties from ICollectable interface
+    public bool isCollected { get; set; }
     public void Collect()
     {
-        // Implementation for Collect method
+        // Implementation not provided for this task
     }
-}
 
-class Program
-{
-    static void Main(string[] args)
+    // Method from IInteractive interface
+    public void Interact()
     {
-        TestObject item = new TestObject();
-        Type type = item.GetType();
-
-        Console.WriteLine("Type: " + type);
-
-        Console.WriteLine("Properties:");
-        foreach (var property in type.GetProperties())
-            Console.WriteLine(property.Name);
-
-        Console.WriteLine("Methods:");
-        foreach (var method in type.GetMethods())
-            Console.WriteLine(method.Name);
+        // Implementation not provided for this task
     }
 }
