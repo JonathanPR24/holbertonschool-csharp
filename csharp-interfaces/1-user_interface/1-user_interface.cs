9fccc0d0c2eb3d04162c94fa048e784d5b622224
Jonathan <jonathanperez604@gmail.com>
2024-03-31 16:16:50 -0400

    User interface

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,69 +1,73 @@
 ﻿using System;
 
-///<summary>Base class representing an entity.</summary>
+///<summary>Base class</summary>
 abstract class Base
 {
     public string name = "";
 
-    ///<summary>Returns a string representation of the entity.</summary>
-    ///<returns>A string representing the name and type of the entity.</returns>
     public override string ToString()
     {
-        return $"{name} is a {this.GetType().ToString()}";
+        return $"{name} is a {GetType()}";
     }
 }
 
 ///<summary>Interactive interface</summary>
 interface IInteractive
 {
-    ///<summary>Interacts with the object.</summary>
     void Interact();
 }
 
-///<summary>Breakable Interface</summary>
+///<summary>Breakable interface</summary>
 interface IBreakable
 {
-    ///<summary>The durability of the object.</summary>
     int durability { get; set; }
-
-    ///<summary>Breaks the object.</summary>
     void Break();
 }
 
-///<summary>Collectable Interface</summary>
+///<summary>Collectable interface</summary>
 interface ICollectable
 {
-    ///<summary>Indicates whether the object has been collected.</summary>
     bool isCollected { get; set; }
-
-    ///<summary>Collects the object.</summary>
     void Collect();
 }
 
 ///<summary>Class that inherits from Base class and all interfaces</summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    ///<summary>The durability of the object.</summary>
     public int durability { get; set; }
-
-    ///<summary>Indicates whether the object has been collected.</summary>
     public bool isCollected { get; set; }
 
-    ///<summary>Interacts with the object.</summary>
     public void Interact()
     {
         // Implementation for Interact method
     }
 
-    ///<summary>Breaks the object.</summary>
     public void Break()
     {
         // Implementation for Break method
     }
 
-    ///<summary>Collects the object.</summary>
     public void Collect()
     {
         // Implementation for Collect method
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TestObject item = new TestObject();
+        Type type = item.GetType();
+
+        Console.WriteLine("Type: " + type);
+
+        Console.WriteLine("Properties:");
+        foreach (var property in type.GetProperties())
+            Console.WriteLine(property.Name);
+
+        Console.WriteLine("Methods:");
+        foreach (var method in type.GetMethods())
+            Console.WriteLine(method.Name);
     }
 }
