1d4d71e397f0ff19f34decc09ec50a47e43fe06b
Jonathan <jonathanperez604@gmail.com>
2024-03-31 19:34:29 -0400

    Damage delegation

--- csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 
-public delegate void CalculateHealth(float amount);
+delegate void CalculateHealth(float value);
 
+/// <summary>
+/// Represents a character in the game.
+/// </summary>
 public class Player
 {
-    public string name { get; set; }
-    public float maxHp { get; set; }
-    public float hp { get; set; }
+    string name { get; set; }
+    float maxHp { get; set; }
+    float hp { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the Player class.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health points of the player.</param>
     public Player(string name = "Player", float maxHp = 100f) {
         this.name = name;
+
         if (maxHp > 0) {
             this.maxHp = maxHp;
         } else {
@@ -19,37 +28,36 @@ public class Player
         this.hp = this.maxHp;
     }
 
+    /// <summary>
+    /// Applies damage to the player.
+    /// </summary>
+    /// <param name="damage">The amount of damage to be applied.</param>
+    public void TakeDamage(float damage) {
+        if (damage >= 0) {
+            hp -= damage;
+            Console.WriteLine($"{name} takes {damage} damage!");
+        } else {
+            Console.WriteLine($"{name} takes 0 damage!");
+        }
+    }
+
+    /// <summary>
+    /// Rejuvenates player's HP.
+    /// </summary>
+    /// <param name="heal">The amount of HP to be healed.</param>
+    public void HealDamage(float heal) {
+        if (heal >= 0) {
+            hp += heal;
+            Console.WriteLine($"{name} heals {heal} HP!");
+        } else {
+            Console.WriteLine($"{name} heals 0 HP!");
+        }
+    }
+
+    /// <summary>
+    /// Prints the health of the player.
+    /// </summary>
     public void PrintHealth() {
         Console.WriteLine($"{name} has {hp} / {maxHp} health");
-    }
-
-    public void TakeDamage(float damage) {
-        if (damage < 0) {
-            damage = 0;
-        }
-        hp -= damage;
-        Console.WriteLine($"{name} takes {damage} damage!");
-    }
-
-    public void HealDamage(float heal) {
-        if (heal < 0) {
-            heal = 0;
-        }
-        hp += heal;
-        Console.WriteLine($"{name} heals {heal} HP!");
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Player player = new Player("Electric Mouse");
-
-        player.TakeDamage(25f);
-
-        player.HealDamage(10f);
-
-        player.TakeDamage(-25f);
     }
 }
