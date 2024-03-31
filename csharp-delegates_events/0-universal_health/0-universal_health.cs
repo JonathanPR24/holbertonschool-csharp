b943b12fb7ed465418de7fada56162af5459f10f
Jonathan <jonathanperez604@gmail.com>
2024-03-31 19:26:52 -0400

    Universal health

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,36 +1,38 @@
 ﻿﻿using System;
 
-/// <summary>
-/// Represents an entity in the game.
-/// </summary>
 public class Player
 {
-    string name { get; set; }
-    float maxHp { get; set; }
-    float hp { get; set; }
+    private string name;
+    private float maxHp;
+    private float hp;
 
-    /// <summary>
-    /// Initializes a new instance of the Player class.
-    /// </summary>
-    /// <param name="name">The name of the player.</param>
-    /// <param name="maxHp">The maximum health points of the player.</param>
-    public Player(string name = "Player", float maxHp = 100f) {
-        this.name = name;
-
-        if (maxHp > 0) {
-            this.maxHp = maxHp;
-        } else {
-            this.maxHp = 100f;
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
         }
-        this.hp = this.maxHp;
+
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
     }
 
-    /// <summary>
-    /// Prints the current health of the player.
-    /// </summary>
-    public void PrintHealth() {
+    public void PrintHealth()
+    {
         Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
+}
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player1 = new Player("Electric Mouse", 500f);
+        Player player2 = new Player("Water Turtle");
+
+        player1.PrintHealth();
+        player2.PrintHealth();
+    }
 }
