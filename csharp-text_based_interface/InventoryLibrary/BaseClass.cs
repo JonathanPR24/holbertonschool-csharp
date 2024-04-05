62358d0b4212e612898f8bd0857099e3715e149b
Jonathan <jonathanperez604@gmail.com>
2024-04-05 13:47:42 -0400

    Base knowledge

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        // Constructor
+        public BaseClass()
+        {
+            Id = Guid.NewGuid().ToString(); // Generate a unique ID
+            DateCreated = DateTime.Now;
+            DateUpdated = DateTime.Now;
+        }
+    }
+}
